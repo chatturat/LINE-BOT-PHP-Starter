4499b1fc285fad5d19a4e8af84dc1e3208195d20
chatturat <35519372+chatturat@users.noreply.github.com>
2018-01-19 17:34:20 +0700

    Update verify.php

--- verify.php
+++ verify.php
@@ -3,7 +3,7 @@ $access_token = 'XXXXXXXXXXXXXXXXXXXXXXX';
 
 $url = 'https://api.line.me/v1/oauth/verify';
 
-$headers = array('Authorization: Bearer ' . $access_token);
+$headers = array('Authorization: Bearer ' . $UmFNhzXE0Zc07TRxM/tS1Ob+GL8r+J1g/pbWFRdDEdNdoXXmFE53rSnERCpp9h3DWOL0IWWjWr1FLxn0F5zXir06qyn0fINVLkYcl81CqGRzjC/8I5pvxZzDDIxenqnqpnF8W+6eupIcFpsVQnubdQdB04t89/1O/w1cDnyilFU=);
 
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
