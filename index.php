11fe31d5bc9a31b52d7d00c01384d5f1935c53fd
chatturat <35519372+chatturat@users.noreply.github.com>
2018-03-06 17:12:23 +0700

    Update index.php

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
         $messages = [	'type' => 'text',	'text' => $text	];			
         // Make a POST Request to Messaging API to reply to sender			
         $url = 'https://api.line.me/v2/bot/message/reply';			
-        $data = [	'replyToken' => $replyToken,'messages' => [$messages],];			
+        $data = ['replyToken' => $replyToken,'messages' => [$messages],];			
         $post = json_encode($data);			
         $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);			
         $ch = curl_init($url);			
