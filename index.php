b942745f9723d86047866b358235ee5156838df6
chatturat <35519372+chatturat@users.noreply.github.com>
2018-03-08 09:35:23 +0700

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,8 @@
       // Reply only when message sent is in 'text' format		
       if ($event['type'] == 'message' && $event['message']['type'] == 'text') {			
         // Get text sent			
-        $text = $event['message']['text'];			
+        $text = $event['message']['text'];
+        $text = $event['สวัสดี']['text'];
         // Get replyToken			
         $replyToken = $event['replyToken'];			
         // Build message to reply back			
