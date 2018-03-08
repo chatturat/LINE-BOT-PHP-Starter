b2e9b769b3cb65ef1c3536a4f928dd072a10b880
chatturat <35519372+chatturat@users.noreply.github.com>
2018-03-08 09:48:03 +0700

    Update index.php

--- index.php
+++ index.php
@@ -12,15 +12,13 @@
       if ($event['type'] == 'message' && $event['message']['type'] == 'text') {			
         // Get text sent			
         $text = $event['message']['text'];
-        $text1 = $event['Hello']['text'];
         // Get replyToken			
         $replyToken = $event['replyToken'];			
         // Build message to reply back			
         $messages = [	'type' => 'text',	'text' => $text	];
-        $messages1 = [	'type' => 'text',	'text' => $text1	];
         // Make a POST Request to Messaging API to reply to sender			
         $url = 'https://api.line.me/v2/bot/message/reply';			
-        $data = ['replyToken' => $replyToken,'messages' => [$messages],[$messages1]];			
+        $data = ['replyToken' => $replyToken,'messages' => [$messages]];			
         $post = json_encode($data);			
         $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);			
         $ch = curl_init($url);			
